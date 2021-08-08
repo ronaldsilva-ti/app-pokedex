91a4d701fbe545ef51750c2987d63e7d7027c71e
ronalddsilva <ronald@rbmweb.com.br>
2021-08-07 21:27:35 -0300

    feat:customed bottom tabs

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,53 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import Home from './modules/Home';
 const Tab = createBottomTabNavigator();
 
+const icons = {
+  Home: {
+    lib: MaterialCommunityIcons,
+    name: 'home',
+  },
+  Plus: {
+    lib: AntDesign,
+    name: 'plus',
+  },
+};
+
 const Navigation = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Mais" component={Home} />
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        // eslint-disable-next-line react/display-name
+        tabBarIcon: ({ color, size, focused }) => {
+          const { lib: Icon, name } = icons[route.name];
+          return <Icon name={name} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: '#fff',
+        // tabBarBackground: '#131418',
+        tabBarStyle: { backgroundColor: '#000', borderTopColor: '#000' },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'home',
+        }}
+      />
+      <Tab.Screen
+        name="Plus"
+        component={Home}
+        options={{
+          title: 'plus',
+        }}
+      />
     </Tab.Navigator>
   );
 };
